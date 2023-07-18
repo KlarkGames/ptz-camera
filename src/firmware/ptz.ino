85885e85693da846304afa1da86c13e85aa2023e
Alex Laletin <alex.klark.laletin@yandex.com>
2023-07-18 10:40:49 +0300

    feat: arduino firmware updated

--- src/firmware/ptz.ino
+++ src/firmware/ptz.ino
@@ -1,3 +1,8 @@
+#define LEFT 1
+#define RIGHT 0
+#define UP 1
+#define DOWN 0
+
 const int enPin=8;
 const int stepXPin = 2; //X.STEP
 const int dirXPin = 5; // X.DIR
@@ -5,6 +10,11 @@ const int stepYPin = 3; //Y.STEP
 const int dirYPin = 6; // Y.DIR
 const int stepZPin = 4; //Z.STEP
 const int dirZPin = 7; // Z.DIR
+
+bool left = false;
+bool right = false;
+bool up = false;
+bool down = false;
 
 const char separator = ':';
 int pulseWidthMicros = 100;  // microseconds
@@ -22,36 +32,6 @@ void setup() {
   Serial.println(F("CNC Shield Initialized"));
 }
 
-boolean isValidNumber(String str){
-  for(byte i=0;i<str.length();i++)
-  {
-    if(isDigit(str.charAt(i))) return true;
-  }
-
-  return false;
-}
-
-int getValue(String data, int index)
-{
-    int found = 0;
-    int strIndex[] = { 0, -1 };
-    int maxIndex = data.length() - 1;
-
-    for (int i = 0; i <= maxIndex && found <= index; i++) {
-        if (data.charAt(i) == separator || i == maxIndex) {
-            found++;
-            strIndex[0] = strIndex[1] + 1;
-            strIndex[1] = (i == maxIndex) ? i+1 : i;
-        }
-    }
-
-    String strValue = data.substring(strIndex[0], strIndex[1]);
-    bool isNumericString = isValidNumber(strValue);
-    Serial.println("Value:" + strValue + ".");
-    Serial.println("IsNumericString:" + isNumericString);
-    return found && isNumericString > index ? strValue.toInt() : -1;
-}
-
 void rotateFunc(int stepPin) {
   digitalWrite(stepPin, HIGH);
   delayMicroseconds(millisBtwnSteps);
@@ -59,56 +39,58 @@ void rotateFunc(int stepPin) {
   delayMicroseconds(millisBtwnSteps);
 }
 
-void rotate(int dirPin, int stepPin, int direction, int steps) {
-  Serial.println(F("Running clockwise"));
-  digitalWrite(dirPin, direction); // Enables the motor to move in a particular direction
-  // Makes 200 pulses for making one full cycle rotation
-  for (int i = 0; i < steps; i++) {
-    rotateFunc(stepPin);
-  }
-  delay(1000); // One second delay
-}
 
-// Example message: "5 2 1 200" 
+// Example message: "UL" 
 void loop() {
   if (Serial.available() > 0) {
-    String dirPinValue = Serial.readStringUntil(' ');
-    if (!isValidNumber(dirPinValue)){
-      Serial.println("Direction pin value is not a number: " + dirPinValue + ".");
-      return;
+
+    String line = Serial.readString();
+    int direction = line.charAt(0);
+    int command = line.charAt(1);
+
+    int value = (command == 'L') ? true : false;
+
+    switch(direction)
+    {
+      case 'L': {
+        left = value;
+        if (value == true) right = false;
+        break;
+      }
+      case 'R': {
+        right = value;
+        if (value == true) left = false;
+        break;
+      }
+      case 'U': {
+        up = value;
+        if (value == true) down = false;
+        break;
+      }
+      case 'D': {
+        down = value;
+        if (value == true) up = false;
+        break;
+      }
     }
-    Serial.read();
-    int dirPin = dirPinValue.toInt();
 
-    String stepPinValue = Serial.readStringUntil(' ');
-    if (!isValidNumber(stepPinValue)){
-      Serial.println("Step pin value is not a number: " + stepPinValue + ".");
-      return;
+} else {
+    if (left == true) {
+      digitalWrite(dirXPin, LEFT);
+      rotateFunc(stepXPin);
     }
-    Serial.read();
-    int stepPin = stepPinValue.toInt();
-
-    String directionValue = Serial.readStringUntil(' ');
-    if (!isValidNumber(directionValue)){
-      Serial.println("Direction value is not a number: " + directionValue + ".");
-      return;
+    if (right == true) {
+      digitalWrite(dirXPin, RIGHT);
+      rotateFunc(stepXPin);
     }
-    Serial.read();
-    int direction = directionValue.toInt();
-
-    String stepsValue = Serial.readStringUntil(' ');
-    if (!isValidNumber(stepsValue)){
-      Serial.println("Steps value is not a number: " + stepsValue + ".");
-      return;
+    if (up == true) {
+      digitalWrite(dirYPin, UP);
+      rotateFunc(stepYPin);
     }
-    Serial.read();
-    int steps = stepsValue.toInt();
-
-    Serial.println(dirPin);
-    Serial.println(stepPin);
-    Serial.println(direction);
-    Serial.println(steps);
-
-    rotate(dirPin, stepPin, (direction==0)?HIGH:LOW, steps);
+    if (down == true) {
+      digitalWrite(dirYPin, DOWN);
+      rotateFunc(stepYPin);
+    }
+    //rotate(dirPin, stepPin, (direction==0)?HIGH:LOW, steps);
   }
 }
