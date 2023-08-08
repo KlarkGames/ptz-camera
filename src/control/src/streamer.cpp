e271706b08c5915c3f976b22d37a02a543ac52ed
snek250721 <murad.masimov@gmail.com>
2023-08-08 22:39:43 +0500

    feat: camera device can be changed while streaming

--- src/control/src/streamer.cpp
+++ src/control/src/streamer.cpp
@@ -10,7 +10,7 @@ bool Streamer::initStreaming(QHostAddress address, QString cameraDevice)
 {
     GError *err = NULL;
     QString cmd = QString(
-        "v4l2src device=%1 ! tee name=t0 ! queue max-size-buffers=1000 leaky=downstream ! jpegenc ! \
+        "v4l2src name=v4l2src0 device=%1 ! tee name=t0 ! queue max-size-buffers=1000 leaky=downstream ! jpegenc ! \
          appsink drop=true async=false max-buffers=30 name=appsink0 t0. ! queue max-size-buffers=1000 leaky=downstream ! \
          videoconvert ! video/x-raw,format=I420 ! \
          x264enc tune=zerolatency speed-preset=superfast key-int-max=12 byte-stream=true ! \
@@ -25,6 +25,7 @@ bool Streamer::initStreaming(QHostAddress address, QString cameraDevice)
         return false;
     }
 
+    m_src = gst_bin_get_by_name(GST_BIN(m_pipeline), "v4l2src0");
     m_appsink = gst_bin_get_by_name(GST_BIN(m_pipeline), "appsink0");
     m_filesink = gst_bin_get_by_name(GST_BIN(m_pipeline), "filesink0");
 
@@ -68,6 +69,15 @@ bool Streamer::initStreaming(QHostAddress address, QString cameraDevice)
     }
 
     return true;
+}
+
+void Streamer::setCameraDevice(QString cameraDevice)
+{
+    gst_element_set_state(m_src, GST_STATE_NULL);
+    g_object_set(m_src, "device", cameraDevice.toUtf8().constData(), NULL);
+    gst_element_set_state(m_src, GST_STATE_PLAYING);
+
+    emit cameraDeviceChanged(cameraDevice);
 }
 
 void Streamer::setRecording(bool value)
